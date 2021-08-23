ba0da89602327e3008b3ccecda50108a1249c7dc
Hasssan Ahmed <ahmed.hassan.112.ha@gmail.com>
2021-08-23 19:17:37 +0500

    signup front end and client-side verification done

--- app/src/main/java/com/fyp/al_saffar/Signup.java
+++ app/src/main/java/com/fyp/al_saffar/Signup.java
@@ -3,8 +3,22 @@ package com.fyp.al_saffar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.fyp.al_saffar.models.UserCreationModel;
 
 public class Signup extends AppCompatActivity {
+
+    EditText usernameET;
+    EditText emailAddressET;
+    EditText passwordET;
+    EditText confirmPasswordET;
+    Button signUpButton;
+    final String TAG = "XXXXXXXXXXXXXXXX";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +26,64 @@ public class Signup extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        usernameET = findViewById(R.id.input_username_signup);
+        emailAddressET = findViewById(R.id.input_email_address_signup);
+        passwordET = findViewById(R.id.input_password_signup);
+        confirmPasswordET = findViewById(R.id.input_confirm_password_signup);
+        signUpButton = findViewById(R.id.btn_signup);
+        signUpButton.setOnClickListener(this::signUpButtonClicked);
+
+    }
+
+    public void signUpButtonClicked(View v) {
+        UserCreationModel userData = extractDataFromForm();
+
+        // Set everything back to what it was
+        usernameET.setBackgroundColor(getResources().getColor(R.color.subtitle_background));
+        emailAddressET.setBackgroundColor(getResources().getColor(R.color.subtitle_background));
+        passwordET.setBackgroundColor(getResources().getColor(R.color.subtitle_background));
+        confirmPasswordET.setBackgroundColor(getResources().getColor(R.color.subtitle_background));
+
+        // Check for information not provided
+        if (userData.basicInfoValidated && userData.errors.size() != 0) {
+            for (int i = 0; i < userData.errors.size(); i++) {
+                int errorCode = userData.errors.get(i);
+                switch (errorCode) {
+                    case 0:
+                        usernameET.setBackgroundColor(getResources().getColor(R.color.subtitle));
+                        break;
+                    case 1:
+                        emailAddressET
+                                .setBackgroundColor(getResources().getColor(R.color.subtitle));
+                        break;
+                    case 2:
+                        passwordET.setBackgroundColor(getResources().getColor(R.color.subtitle));
+                        break;
+
+                    case 3:
+                        confirmPasswordET
+                                .setBackgroundColor(getResources().getColor(R.color.subtitle));
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return;
+
+        }
+
+        // Check for other errors
+        if (userData.errorStatus != 0) {
+            Toast.makeText(this, userData.errorMessages[userData.errorStatus], Toast.LENGTH_SHORT)
+                    .show();
+            return;
+        }
+    }
+
+    public UserCreationModel extractDataFromForm() {
+        return new
+                UserCreationModel(usernameET.getText().toString(),
+                emailAddressET.getText().toString(), passwordET.getText().toString(),
+                confirmPasswordET.getText().toString());
     }
 }
