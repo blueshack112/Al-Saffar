62c839930e750f28709aaf73d9308bef4af3de84
Hasssan Ahmed <ahmed.hassan.112.ha@gmail.com>
2022-01-02 18:24:30 +0500

    SIGNUP COMPLETE

--- app/src/main/java/com/fyp/al_saffar/Signup.java
+++ app/src/main/java/com/fyp/al_saffar/Signup.java
@@ -2,13 +2,34 @@ package com.fyp.al_saffar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.fyp.al_saffar.models.UserCreationModel;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.fyp.al_saffar.Values.API_ACCOUNTS;
 
 public class Signup extends AppCompatActivity {
 
@@ -91,6 +112,60 @@ public class Signup extends AppCompatActivity {
         }
 
         // Send api call to signup
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = Values.API_ORIGIN + Values.API_ACCOUNTS;
+
+        // Request a string response from the provided URL.
+        StringRequest createAccountCall = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        JSONObject jsonResponse = null;
+                        try {
+                            jsonResponse = new JSONObject(response.toString());
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (!success) {
+                                JSONObject jsonErrors = jsonResponse.getJSONObject("errors");
+                                String errorKey = jsonErrors.keys().next();
+                                String errorMessage =
+                                        jsonErrors.getJSONArray(errorKey).get(0).toString();
+                                Toast.makeText(Signup.this, errorMessage, Toast.LENGTH_LONG)
+                                        .show();
+                            } else {
+                                Toast.makeText(Signup.this, "Account created!", Toast.LENGTH_LONG)
+                                        .show();
+                                goToLogin();
+                            }
+                        } catch (JSONException e) {
+                            Toast.makeText(Signup.this, "Error Occured: Try again later",
+                                    Toast.LENGTH_LONG).show();
+                            return;
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, error.getLocalizedMessage());
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> param = new HashMap<>();
+                //Put Data
+                param.put("username", userData.username);
+                param.put("email", userData.emailAddress);
+                param.put("password1", userData.password);
+                param.put("password2", userData.confirmPassword);
+                param.put("fname", userData.firstName);
+                param.put("lname", userData.lastName);
+                return param;
+            }
+        };
+
+
+        queue.add(createAccountCall);
     }
 
     public UserCreationModel extractDataFromForm() {
@@ -98,5 +173,10 @@ public class Signup extends AppCompatActivity {
                 UserCreationModel(usernameET.getText().toString(), firstNameET.getText().toString(),
                 lastNameET.getText().toString(), emailAddressET.getText().toString(),
                 passwordET.getText().toString(), confirmPasswordET.getText().toString());
+    }
+
+    public void goToLogin() {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/fyp/al_saffar/Values.java
@@ -0,0 +1,8 @@
+package com.fyp.al_saffar;
+
+public class Values {
+    public static String API_ORIGIN = "http://192.168.1.3/";
+    public static String API_ACCOUNTS = "accounts/";
+    public static String API_TEST = "test/";
+//    public static String API_ORIGIN = "localhost/";
+}
