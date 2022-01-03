cb5596c33015c42e8b004fb0c072ddc89e57ba15
Hasssan Ahmed <ahmed.hassan.112.ha@gmail.com>
2022-01-03 22:51:16 +0500

    final change

--- app/src/main/java/com/fyp/al_saffar/Values.java
+++ app/src/main/java/com/fyp/al_saffar/Values.java
@@ -1,9 +1,12 @@
 package com.fyp.al_saffar;
 
 public class Values {
-    public static final String TAG = "XXXXXXXXXXXXXXXX";
+    // TODO:
     public static String API_ORIGIN = "http://192.168.1.3/";
     public static String API_ORIGIN_WITHOUT_HTTP = "192.168.1.3/";
+    // TODO:
+
+    public static final String TAG = "XXXXXXXXXXXXXXXX";
     public static String API_ACCOUNTS = "accounts/";
     public static String API_ACCOUNTS_LOGIN = "login/";
     public static String API_ACCOUNTS_CHANGE_PASS = "changepass/";
