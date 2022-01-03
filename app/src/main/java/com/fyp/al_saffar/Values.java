a4937e0be862173c57ac215b16339bb407f4d753
Hasssan Ahmed <ahmed.hassan.112.ha@gmail.com>
2022-01-03 17:04:25 +0500

    add value

--- app/src/main/java/com/fyp/al_saffar/Values.java
+++ app/src/main/java/com/fyp/al_saffar/Values.java
@@ -3,14 +3,25 @@ package com.fyp.al_saffar;
 public class Values {
     public static final String TAG = "XXXXXXXXXXXXXXXX";
     public static String API_ORIGIN = "http://192.168.1.3/";
+    public static String API_ORIGIN_WITHOUT_HTTP = "192.168.1.3/";
     public static String API_ACCOUNTS = "accounts/";
     public static String API_ACCOUNTS_LOGIN = "login/";
     public static String API_CARS = "cars/";
     public static String API_TEST = "test/";
 
     // Shared Preferences Stuff
-    public static String SP_FILE_KEY = "sp_add_data";
+    public static String SP_FILE_KEY = "sp_user_data";
     public static String SPF_USER_ID_KEY = "spf_user_id";
+
+    public static String SPF_CAR_ID_KEY = "car_user_id";
+    public static String SPF_CAR_BRAND_KEY = "car_brand";
+    public static String SPF_CAR_MAKE_KEY = "car_make";
+    public static String SPF_CAR_MODEL_KEY = "car_model";
+    public static String SPF_CAR_YEAR_KEY = "car_year";
+    public static String SPF_CAR_CAPACITY_KEY = "car_capacity";
+    public static String SPF_CAR_CONDITION_KEY = "car_condition";
+    public static String SPF_CAR_APPROVED_KEY = "car_approved";
+
 
 //    public static String API_ORIGIN = "localhost/";
 }
