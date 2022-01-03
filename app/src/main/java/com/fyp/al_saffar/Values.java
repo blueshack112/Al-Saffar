c9bda58407c510e0258dc2011e3726e42476f80e
Hasssan Ahmed <ahmed.hassan.112.ha@gmail.com>
2022-01-03 16:25:52 +0500

    new values

--- app/src/main/java/com/fyp/al_saffar/Values.java
+++ app/src/main/java/com/fyp/al_saffar/Values.java
@@ -5,6 +5,12 @@ public class Values {
     public static String API_ORIGIN = "http://192.168.1.3/";
     public static String API_ACCOUNTS = "accounts/";
     public static String API_ACCOUNTS_LOGIN = "login/";
+    public static String API_CARS = "cars/";
     public static String API_TEST = "test/";
+
+    // Shared Preferences Stuff
+    public static String SP_FILE_KEY = "sp_add_data";
+    public static String SPF_USER_ID_KEY = "spf_user_id";
+
 //    public static String API_ORIGIN = "localhost/";
 }
