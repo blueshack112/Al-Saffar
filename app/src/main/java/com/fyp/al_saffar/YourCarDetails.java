b9bac16293a70b973c47f56071a28d4cf6cc6acf
Hasssan Ahmed <ahmed.hassan.112.ha@gmail.com>
2022-01-03 17:15:34 +0500

    add car details activity

---
+++ app/src/main/java/com/fyp/al_saffar/YourCarDetails.java
@@ -0,0 +1,14 @@
+package com.fyp.al_saffar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class YourCarDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_your_car_details);
+    }
+}
